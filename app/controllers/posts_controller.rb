30832fcce84cd1b01d21c83d7f1f2a48f63f8355
ravimathya <ravimathya.gmail.com>
2019-10-21 14:16:43 +0545

    before adding comment model and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create]
+    before_action :is_owner?, only: [:edit, :update, "destroy"]
 
     def index
         @posts = Post.all.order('created_at DESC')
@@ -13,8 +14,21 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
 
+    def show
+      @post = Post.find(params[:id])
+    end
+
+    def create
+      @post = current_user.posts.create(post_params)
+      if @post.valid?
+        redirect_to root_path
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
     def update
-        @post = Post.fine(params[:id])
+        @post = Post.find(params[:id])
         @post.update(post_params)
         if @post.valid?
             redirect_to root_path
@@ -23,18 +37,21 @@ class PostsController < ApplicationController
         end
     end
 
-    def create
-        @post = current_user.posts.create(post_params)
-        if @post.valid?
-          redirect_to root_path
-        else
-          render :new, status: :unprocessable_entity
-        end
-      end
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+      redirect_to root_path
+    end
       
-      private
-      
-      def post_params
-        params.require(:post).permit(:user_id, :photo, :description)
+    private
+
+    def is_owner?
+      if Post.find(params[:id]).user != current_user
+        redirect_to root_path
       end
+    end
+    
+    def post_params
+      params.require(:post).permit(:user_id, :photo, :description)
+    end
 end
