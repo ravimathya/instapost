2466f834232543666c71e98ceb39c9e6e193dbae
ravimathya <ravimathya.gmail.com>
2019-10-21 15:16:01 +0545

    commenting added to front-end

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
     mount_uploader :photo, PhotoUploader
     belongs_to :user
+    has_many :comment
     validates :photo, :description, :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :photo, PhotoUploader
   has_many :posts
+  has_many :comments
 end
